9e6ca7661ffee5d465e7d2e058880a438f18fcb1
Thomas Hill <tomhill555@gmail.com>
2023-03-09 22:28:00 +0000

    comlpeted demo

---
+++ src/main/java/com/example/SpringBatchDemo/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.example.SpringBatchDemo.repository;
+
+import com.example.SpringBatchDemo.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
